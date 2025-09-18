143f4c2e825076094c2e579e9d1f7ef68fe36251
Rampravesh Rana <rampraveshrana1435@gmail.com>
2025-09-18 22:45:43 +0530

    Free GOOGLE GEMINI API Integration

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ const messageInput = document.querySelector(".message-input");
 const chatBody = document.querySelector(".chat-body");
 const sendMessageButton = document.querySelector("#send-message");
 
+//API Setup
+const API_KEY = `AIzaSyBz83pJNHx11nIitwUvKX1D_Zz7AJ47824`;
+const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${API_KEY}`;
+
 const userData = {
     message: null
 }
@@ -12,6 +16,32 @@ const createMessageElement = (content, ...classes) => {
     div.classList.add("message", ...classes);
     div.innerHTML = content;
     return div;
+}
+
+//Generate bot response using API
+const generateBotResponse = async () => {
+
+    //API request options
+    const requestOptions = {
+        method: "POST",
+        headers: { "Content-Type" : "application/json" },
+        body: JSON.stringify({
+            contents: [{
+                "parts": [{text: userData.message }]
+            }]
+        })
+    }
+
+    try {
+        //fetch bot response form API
+        const response = await fetch(API_URL, requestOptions);
+        const data = await response.json();
+        if(!response.ok) throw new Error(data.error.message);
+
+        console.log(data);
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 //Handle outgoing user message
@@ -43,6 +73,7 @@ const handleOutgoingMessage = (e) => {
 
         const incomingMessageDiv = createMessageElement(messageContent, "bot-message","thinking");
         chatBody.appendChild(incomingMessageDiv);
+        generateBotResponse();
     }, 600);
 }
 
