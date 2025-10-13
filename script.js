4ff94fcdb1af1117d18971da1b545790b1db3586
Rampravesh Rana <rampraveshrana1435@gmail.com>
2025-10-13 23:53:45 +0530

    Initialize imoji picker

--- script.js
+++ script.js
@@ -136,6 +136,27 @@ fileCancelButton.addEventListener("click", () => {
     fileUploadWrapper.classList.remove("file-uploaded");
 });
 
+//Initialize imoji picker
+const picker = new EmojiMart.Picker({
+    theme: "light",
+    skinTonePosition: "none",
+    previewPosition: "none",
+    onEmojiSelect: (emoji) => {
+        const { selectionStart: start, selectionEnd: end } = messageInput;
+        messageInput.setRangeText(emoji.native, start, end, "end");
+        messageInput.focus();
+    },
+    onClickOutside: (e) => {
+        if(e.target.id === "emoji-picker") {
+            document.body.classList.toggle("show-emoji-picker");
+        } else {
+            document.body.classList.remove("show-emoji-picker");
+        }
+    }
+});
+
+document.querySelector(".chat-form").appendChild(picker);
+
 sendMessageButton.addEventListener("click", (e) => handleOutgoingMessage(e))
 
 document.querySelector("#file-upload").addEventListener("click", () => fileInput.click());
