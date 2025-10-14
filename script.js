1e4c03de14e5c0aef7a023bc8a745d4252c895e0
Rampravesh Rana <rampraveshrana1435@gmail.com>
2025-10-14 23:22:52 +0530

    Update in toggling and close the chatbot popup

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const fileInput = document.querySelector("#file-input");
 const fileUploadWrapper = document.querySelector(".file-upload-wrapper");
 const fileCancelButton = document.querySelector("#file-cancel");
 const chatbotToggler = document.querySelector("#button-toggler");
+const closeChatbot = document.querySelector("#close-chatbot");
 
 //API Setup
 const API_KEY = "AIzaSyBz83pJNHx11nIitwUvKX1D_Zz7AJ47824";
@@ -17,6 +18,8 @@ const userData = {
         mime_type: null
     }
 }
+
+const initialInputHeight = messageInput.scrollHeight;
 
 //Create message element with dynamic classes and return it
 const createMessageElement = (content, ...classes) => {
@@ -69,6 +72,7 @@ const handleOutgoingMessage = (e) => {
     userData.message = messageInput.value.trim();
     messageInput.value = "";
     fileUploadWrapper.classList.remove("file-uploaded");
+    messageInput.dispatchEvent(new Event("input"));
 
     //Create and display user message
     const messageContent = `<div class="message-text"></div>
@@ -103,9 +107,16 @@ const handleOutgoingMessage = (e) => {
 // Handle enter key press for sending messages
 messageInput.addEventListener("keydown", (e) => {
     const userMessage = e.target.value.trim();
-    if(e.key === "Enter" && userMessage){
+    if(e.key === "Enter" && userMessage && !e.shiftKey && window.innerWidth > 768){
         handleOutgoingMessage(e);
     }
+});
+
+//Adjust input field height dyanamically
+messageInput.addEventListener("input", () => {
+    messageInput.style.height = `${initialInputHeight}px`;
+    messageInput.style.height = `${messageInput.scrollHeight}px`;
+    document.querySelector(".chat-form").style.borderRadius = messageInput.scrollHeight > initialInputHeight ? "15px" : "32px";
 });
 
 //Handle file input change and preview the selected file
@@ -161,3 +172,4 @@ sendMessageButton.addEventListener("click", (e) => handleOutgoingMessage(e))
 document.querySelector("#file-upload").addEventListener("click", () => fileInput.click());
 
 chatbotToggler.addEventListener("click", () => document.body.classList.toggle("show-chatbot"));
+closeChatbot.addEventListener("click", () => document.body.classList.remove("show-chatbot"));
